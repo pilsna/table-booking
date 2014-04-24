0f4ffcd1f3c61360a9f8a0cad75d5600076e5e4a
Lorenzo Setale <koalalorenzo@gmail.com>
2014-04-24 20:51:14 +0200

    Implemented a new class method to get the lat & long from the address.

--- stable/stable/items.py
+++ stable/stable/items.py
@@ -4,6 +4,9 @@
 # http://doc.scrapy.org/en/latest/topics/items.html
 
 from scrapy.item import Item, Field
+import requests
+
+GEO_API_URL = "http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/find/"
 
 
 class RestaurantItem(Item):
@@ -14,3 +17,18 @@ class RestaurantItem(Item):
     longitude = Field()
     url = Field()
     time_table = Field()
+
+    def address_to_lat_lng(self):
+        """
+            Use arcigis.com API to get latitude and longitude from the Address.
+        """
+        if len(self['address']) <= 3:
+            return
+
+        payload = {'text': self['address'], 'f': 'pjson'}
+        r = requests.get(GEO_API_URL, params=payload)
+        data = r.json()
+        if "locations" in data.keys():
+            coords = data['locations'][0]['feature']['geometry']
+            self["latitude"] = coords['x']
+            self["longitude"] = coords['y']
