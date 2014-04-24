a4c0e0d5c2ba7201e3a97e1eb3820422a90eeaaa
Nils Rydh <nils.rydh@gmail.com>
2014-04-24 21:43:45 +0200

    Fixed geolocation and current location marker

--- js/map.js
+++ js/map.js
@@ -22,17 +22,26 @@
     // on it, and add a single marker.
     map.on('locationfound', function(e) {
         var currentLocation = L.latLng(e.latitude, e.longitude);
+        map.setZoom(16);
         map.panTo(currentLocation);
-        map.setZoom(17);
+        
         if (me) {
             me.setLatLng(currentLocation);
         } else {
             var locationIcon = L.icon({
                 iconUrl: './images/1398384698_591255-location2.png',
             });
+            /*
             me = L.marker(currentLocation, {
                 icon: locationIcon
             }).addTo(map);
+*/
+            me = L.circle(currentLocation, e.accuracy, {
+                color: 'red',
+                fillColor: '#f03',
+                fillOpacity: 0.2
+            }).addTo(map);
+
         }
         // And hide the geolocation button
         //geolocate.parentNode.removeChild(geolocate);
